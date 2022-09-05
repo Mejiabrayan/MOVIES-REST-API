e83d71ee5c10866c1f50626ea29365f2e588caf7
mejiabrayan <learnwithbrayan@gmail.com>
2022-09-05 16:00:18 -0700

    updated line #22; password is required

--- models/models.js
+++ models/models.js
@@ -10,8 +10,7 @@ let movieSchema = mongoose.Schema({
     }, Director: {
         Name: String,
         Bio: String,
-        Birth: String,
-        Death: String
+        Birth: String
     },
     Actors: [String],
     ImageURL: String,
@@ -20,36 +19,42 @@ let movieSchema = mongoose.Schema({
 
 let userSchema = mongoose.Schema({
     Username: { type: String, required: true },
-    Password: String,
+    Password: { type: String, required: true },
     Email: { type: String, required: true },
     Birthday: Date,
     FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
-userSchema.statics.hashPassword = (password) => {  // hashes password
+// HASH THE PASSWORD BEFORE SAVING THE USER MODEL TO THE DATABASE
+userSchema.statics.hashPassword = (password) => {
     return bcrypt.hashSync(password, 10);
 };
 
+// COMPARE THE HASHED PASSWORD IN THE DATABASE WITH THE ONE PROVIDED BY THE USER WHEN THEY LOG IN
 userSchema.methods.validatePassword = function (password) {
     return bcrypt.compareSync(password, this.Password);
 };
+
+// SCHEMA FOR DIRECTORS
 let directorSchema = mongoose.Schema({
     Name: { type: String, required: true },
     Bio: { type: String, required: true },
     Birth: { type: String, required: true }
 })
 
-
+/// SCHEMA FOR GENRES
 let genreSchema = mongoose.Schema({
     Name: { type: String, required: true },
     Description: { type: String, required: true }
 })
 
+// CREATE MODELS FROM SCHEMAS 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
 let Director = mongoose.model('Director', directorSchema);
 let Genre = mongoose.model('Genre', genreSchema);
 
+// EXPORT MODELS
 module.exports.Movie = Movie;
 module.exports.User = User;
 module.exports.Director = Director;
