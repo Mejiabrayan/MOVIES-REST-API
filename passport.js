1918aa8d3b06deb16c421fd0cf6b0b6960951fc0
mejiabrayan <learnwithbrayan@gmail.com>
2022-09-05 15:56:32 -0700

    added template literal to line #14

--- passport.js
+++ passport.js
@@ -11,7 +11,7 @@ passport.use(new LocalStrategy({
     usernameField: 'Username',
     passwordField: 'Password'
 }, (username, password, callback) => {
-    console.log(username + '  ' + password);
+    console.log(`${username}  ${password}`);
     Users.findOne({ Username: username }, (error, user) => {
         if (error) {
             console.log(error);
@@ -21,6 +21,11 @@ passport.use(new LocalStrategy({
         if (!user) {
             console.log('incorrect username');
             return callback(null, false, { message: 'Incorrect username or password.' });
+        }
+
+        if (!user.validatePassword(password)) {
+            console.log('incorrect password');
+            return callback(null, false, { message: 'Incorrect password.' });
         }
 
         console.log('finished');
